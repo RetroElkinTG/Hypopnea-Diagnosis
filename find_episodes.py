64503465de59405774885ef92618001e37c4b653
Elkin Franco <RetroElkinTG@gmail.com>
2024-07-09 19:22:08 +1000

    Added test cases and has_symptom.py

---
+++ find_episodes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ENGG1811 24T2 Assignment 1 
+
+Template file for find_episodes().    
+"""
+
+def find_episodes(disorder_status,min_segment):
+
---
+++ run_diagnostic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ENGG1811 24T2 Assignment 1 
+
+Template file for run_diagnostic().    
+"""
+
+# %% Import  
+import find_episodes as epi
+import flow_rate_to_disorder_status as status
+
+# %% 
+def run_diagnostic(flow_rate,segment_len,interval,threshold,min_segment):
+    
